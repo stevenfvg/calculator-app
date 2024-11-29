a304ec1154bba7c243a760b4d78ca3c57b142f84
Steven Valladares <stevenvalladaresgiron@gmail.com>
2024-11-29 16:45:54 -0400

    style: custom themes text and default props added

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,12 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { globalStyles } from '@/styles/global-styles';
+import ThemeText from '@/components/ThemeText';
 
 const CalculatorApp = () => {
     return (
         <View style={globalStyles.CalculatorContainer}>
-            <Text
-                style={globalStyles.mainResult}
-                numberOfLines={1}
-                adjustsFontSizeToFit
-            >
-                50 x 50
-            </Text>
-            <Text style={globalStyles.subResult}>250</Text>
+            <ThemeText variant="h1">50 + 5000</ThemeText>
+            <ThemeText variant="h2">250</ThemeText>
         </View>
     );
 };
